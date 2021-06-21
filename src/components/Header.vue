105435d685593e705ce3d0bb1391117c8041f023
LucaQuacquarelli <lucaq82@libero.it>
2021-06-21 12:23:28 +0200

    Creazione componenti principali

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        height: 100vh;
+        background-color: green;
+    }
+</style>
